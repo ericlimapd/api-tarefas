1c3a102560359596f269d8456057aaa9d2d224e9
Eric Lima <ericlimapedroso@gmail.com>
2026-07-26 19:43:48 -0300

    feat: conecta as rotas de tarefas e remove código de teste

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,14 @@
-// src/index.ts
 import express from 'express';
+import { tarefaRoutes } from './routes/tarefa.routes';
 
 const app = express();
 const PORTA = 3333;
 
 // Middleware "tradutor" de JSON (CRUCIAL)
-app.use(express.json()); 
+app.use(express.json());
 
-// --- NOSSAS ROTAS ---
-
-// Rota 1: GET na raiz
-app.get('/', (req, res) => {
-  // Não usamos o 'req' aqui
-  
-  // Usamos o 'res' para enviar uma resposta
-  res.status(200).json({ mensagem: 'API funcionando!' });
-});
-
-// Rota 2: POST na raiz
-app.post('/', (req, res) => {
-  // Não usamos o 'req'
-  
-  res.status(201).send('Recebemos seu POST! Obrigado!');
-});
-
+// --- ROTAS ---
+app.use('/tasks', tarefaRoutes);
 // --- FIM DAS ROTAS ---
 
 app.listen(PORTA, () => {
