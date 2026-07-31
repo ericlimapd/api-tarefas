7b081482c93ae76cea8b22475fee0174995d68ea
Eric Lima <ericlimapedroso@gmail.com>
2026-07-31 12:23:09 -0300

    refactor: passa configurações do express para expressConfig.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,6 @@
-import express from 'express';
-import { tarefaRoutes } from './routes/tarefa.routes';
+import { app } from './config/expressConfig';
 
-const app = express();
 const PORTA = 3333;
-
-// Middleware "tradutor" de JSON (CRUCIAL)
-app.use(express.json());
-
-// --- ROTAS ---
-app.use('/tasks', tarefaRoutes);
-// --- FIM DAS ROTAS ---
 
 app.listen(PORTA, () => {
   console.log(`🚀 Servidor rodando na porta ${PORTA}`);
